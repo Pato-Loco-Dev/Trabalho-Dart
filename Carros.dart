19e4941c7cc1d9929a3cf26818f4f245730d09fa
Pato-Loco-Dev <camargoluizfrancisco035@gmail.com>
2024-03-07 16:21:33 -0300

    att

--- Carros.dart
+++ Carros.dart
@@ -12,12 +12,18 @@ main(){
 }
 
 adicionarVeiculo(Map carros, int chaveCarro){
-String? modelo = stdin.readLineSync();
-  int? ano = int.parse(stdin.readLineSync()!);
-  String? litragem = stdin.readLineSync();
-  String? carroceria = stdin.readLineSync();
 
-  Carro newCarro = new Carro(modelo, ano, litragem, carroceria);
+
+  print("Digite o modelo do veículo:");
+  String modelo = stdin.readLineSync()!;
+  print("Digite o ano do veículo:");
+  int ano = int.parse(stdin.readLineSync()!);
+  print("Digite a litragem do veículo:");
+  String litragem = stdin.readLineSync()!;
+  print("Digite a carroceria do veículo:");
+  String carroceria = stdin.readLineSync()!;
+
+  var newCarro = Carro(modelo, ano, litragem, carroceria);
 
   carros[chaveCarro] = newCarro;
   chaveCarro += 1;
@@ -27,7 +33,7 @@ String? modelo = stdin.readLineSync();
   } else {
     print(carros);
     carros.forEach((chaveCarro, Carro) {
-    print('Carro: ${newCarro.modelo}, Ano: ${newCarro.ano}, Litragem: ${newCarro.litragem}, Carroceria: ${newCarro.carroceria}');
+    print('Chave: $chaveCarro, Carro: ${newCarro.modelo}, Ano: ${newCarro.ano}, Litragem: ${newCarro.litragem}, Carroceria: ${newCarro.carroceria}');
   });
   }
   
@@ -37,10 +43,10 @@ String? modelo = stdin.readLineSync();
 
 
 class Carro {
-  String? modelo;
-  int? ano;
-  String? litragem;
-  String? carroceria;
+  String modelo;
+  int ano;
+  String litragem;
+  String carroceria;
 
     Carro(this.modelo, this.ano, this.litragem, this.carroceria);
 }
