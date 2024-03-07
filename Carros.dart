c355dbcf2ad8719cea8d418b5206c5173aba9c87
Pato-Loco-Dev <camargoluizfrancisco035@gmail.com>
2024-03-07 09:34:59 -0300

    "Adicionando carro na lista e printando"

--- Carros.dart
+++ Carros.dart
@@ -4,9 +4,27 @@ import 'dart:collection';
 main(){
 
   Map carros = Map<int, Carro>();
+  int chaveCarro = 0;
 
-  adicionarVeiculo(carros);
+  adicionarVeiculo(carros, chaveCarro);
 
+
+}
+
+adicionarVeiculo(Map carros, int chaveCarro){
+
+  String? modelo = stdin.readLineSync();
+  int? ano = int.parse(stdin.readLineSync()!);
+  String? litragem = stdin.readLineSync();
+  String? carroceria = stdin.readLineSync();
+
+  Carro newCarro = new Carro(modelo, ano, litragem, carroceria);
+
+  carros[chaveCarro] = newCarro;
+
+  carros.forEach((chaveCarro, Carro) {
+    print('Carro: ${newCarro.modelo}, Idade: ${newCarro.ano}, Litragem: ${newCarro.litragem}, Carroceria: ${newCarro.carroceria}');
+  });
 
 }
 
@@ -16,19 +34,5 @@ class Carro {
   String? litragem;
   String? carroceria;
 
-    Carro(){
-      this.modelo = "";
-      this.ano = 0;
-      this.litragem = "";
-      this.carroceria = "";
-
-    }
-}
-
-adicionarVeiculo(Map carros){
-
-  Carro newCarro = new Carro();
-
-
-
+    Carro(this.modelo, this.ano, this.litragem, this.carroceria);
 }
